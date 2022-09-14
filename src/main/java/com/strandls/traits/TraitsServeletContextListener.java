4f9514efa28240ee72fb606f78f5e94e2aef2d49
Harsh Zalavadiya <harsh@strandls.com>
2022-09-14 13:14:57 +0530

    refactor: :recycle: remove sysout

--- src/main/java/com/strandls/traits/TraitsServeletContextListener.java
+++ src/main/java/com/strandls/traits/TraitsServeletContextListener.java
@@ -102,7 +102,6 @@ public class TraitsServeletContextListener extends GuiceServletContextListener {
 
 			for (Annotation annotation : annotations) {
 				if (annotation instanceof javax.persistence.Entity) {
-					System.out.println("Mapping entity :" + cls.getCanonicalName());
 					classes.add(cls);
 				}
 			}
